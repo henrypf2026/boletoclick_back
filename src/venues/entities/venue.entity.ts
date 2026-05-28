b8e9f5c997a611528403c2ce04b9ae04c0a3fa19
Edwin Vivas <jhovannyvivas@gmail.com>
2026-05-28 11:10:58 -0500

    entidad de venues

--- src/venues/entities/venue.entity.ts
+++ src/venues/entities/venue.entity.ts
@@ -1,1 +1,30 @@
-export class Venue {}
+import { Column, PrimaryGeneratedColumn } from 'typeorm';
+
+export class Venue {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @Column({
+    type: 'varchar',
+  })
+  name!: string;
+
+  @Column({
+    type: 'varchar',
+  })
+  address!: string;
+  @Column({ type: 'varchar' })
+  city!: string;
+  @Column({ type: 'integer' })
+  capacity!: number;
+  @Column({ type: 'text', nullable: true })
+  imgUrl!: string | null;
+  @Column({ type: 'decimal', precision: 8, scale: 2 })
+  latitude!: number;
+  @Column({ type: 'decimal', precision: 8, scale: 2 })
+  longitude!: number;
+  @Column({ type: 'timestamptz' })
+  createdAt!: string;
+  @Column({ type: 'timestamptz' })
+  updatedAt!: string;
+}
