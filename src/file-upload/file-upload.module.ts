a8a572761a30f7af1ad0ebce8311d932d4af928b
JONATHAN R. DOMINGUEZ <60529271+JonathanR10@users.noreply.github.com>
2026-05-28 19:14:05 -0600

    Update: updated the file-upload to receive the userId by param

--- src/file-upload/file-upload.module.ts
+++ src/file-upload/file-upload.module.ts
@@ -3,8 +3,10 @@ import { FileUploadService } from './file-upload.service';
 import { FileUploadController } from './file-upload.controller';
 import { CloudinaryConfig } from '../config/cloudinary';
 import { FileUploadRepository } from './file-upload.repository';
+import { UsersModule } from '../users/users.module';
 
 @Module({
+  imports: [UsersModule],
   controllers: [FileUploadController],
   providers: [FileUploadService, CloudinaryConfig, FileUploadRepository],
 })
