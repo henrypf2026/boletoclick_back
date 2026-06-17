22a08afb6982edbdaaaf62d4825ad7a0e4c946d7
Mauricio Ruiz <ruiz85@gmail.com>
2026-06-17 16:12:07 -0600

    fix: remove database query from chatbot

--- src/chatbot/chatbot.module.ts
+++ src/chatbot/chatbot.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ChatbotService } from './chatbot.service';
 import { ChatbotController } from './chatbot.controller';
-import { EventsModule } from '../events/events.module';
 
 @Module({
-  imports: [EventsModule],
   providers: [ChatbotService],
   controllers: [ChatbotController],
 })
