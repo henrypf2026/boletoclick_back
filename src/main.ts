bac32868a2d491a8a37a632fe43b61f0ab593275
jphormiga <crvzado@gmail.com>
2026-06-22 14:31:25 -0500

    no deja crear eventos si la suma de stocks supera el capacity del venue

--- src/main.ts
+++ src/main.ts
@@ -46,7 +46,11 @@ async function bootstrap() {
       .build();
 
     const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api', app, document);
+    SwaggerModule.setup('api', app, document, {
+      swaggerOptions: {
+        withCredentials: true,
+      },
+    });
 
     console.log(`📄 Swagger disponible en /api`);
   }
